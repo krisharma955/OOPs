8020fd06e2a9516b33e53a0a5f28dccf4353088e
Krish Sharma <krishsharma9005@gmail.com>
2025-07-16 11:46:24 +0530

    OOPs

---
+++ classes_objects.java
@@ -0,0 +1,29 @@
+public class classes_objects {
+    static class Employee {
+    String name;
+    int salary;
+
+    public void setName(String s) {
+        name = s;
+    }
+
+    public void setSalary(int newSalary) {
+        salary = newSalary;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+}
+    public static void main(String[] args) {
+        Employee obj1 = new Employee();
+        obj1.setName("Lando");
+        obj1.setSalary(1000);
+        System.out.println(obj1.getSalary());
+
+        Employee obj2 = new Employee();
+        obj2.setName("Lewis");
+        obj2.setSalary(2000);
+        System.out.println(obj2.getSalary());
+    }
+}
