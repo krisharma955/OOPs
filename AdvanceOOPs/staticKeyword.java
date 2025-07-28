a9c8a1de72c6d217dbcc6dcd2546bf9443ff9a8c
Krish Sharma <krishsharma9005@gmail.com>
2025-07-28 12:18:00 +0530

    static keyword

--- AdvanceOOPs/staticKeyword.java
+++ AdvanceOOPs/staticKeyword.java
@@ -20,17 +20,33 @@ public class staticKeyword {
             return balance;
         }
     }
+    class Utility {
+        public static int sum(int a, int b) {
+            return a+b;
+        }
+    }
+    class demo {
+        static int val;
+        static {
+            val = 10;
+            System.out.println("executed");
+        }
+    }
     public static void main(String[] args) {
-        BankAccount obj1 = new BankAccount("Lando", 1000);
-        System.out.println(obj1.getName());
-        System.out.println(obj1.getBalance());
-        System.out.println(obj1.getCounter());
+        // BankAccount obj1 = new BankAccount("Lando", 1000);
+        // System.out.println(obj1.getName());
+        // System.out.println(obj1.getBalance());
+        // System.out.println(obj1.getCounter());
 
-        BankAccount obj2 = new BankAccount("Lewis", 2000);
-        System.out.println(obj2.getName());
-        System.out.println(obj2.getBalance());
-        System.out.println(obj2.getCounter());
+        // BankAccount obj2 = new BankAccount("Lewis", 2000);
+        // System.out.println(obj2.getName());
+        // System.out.println(obj2.getBalance());
+        // System.out.println(obj2.getCounter());
 
-        System.out.println(BankAccount.counter);
+        // System.out.println(BankAccount.counter);
+
+        //System.out.println(Utility.sum(2,3));
+
+        System.out.println(demo.val);
     }
 }
